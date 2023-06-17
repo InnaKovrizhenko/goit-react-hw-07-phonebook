012ce4e7d0ffc91c02cb6b3da99cb7119cbf0a33
InnaKovrizhenko <114367918+InnaKovrizhenko@users.noreply.github.com>
2023-06-17 11:04:49 +0300

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import { ContactForm } from './ContactForm/ContactForm'
+import { Filter } from './Filter/Filter'
+import { ContactList } from './ContactList/ContactList'
+import { Titleh1, Titleh2, Phonebook } from './App.styled'
+import { ToastContainer } from 'react-toastify';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/services/fetchAPI';
+
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+
+const dispatch = useDispatch();
+
+useEffect(() => {
+  dispatch(fetchContacts());
+}, [dispatch]);
+
+
+return (
+  <Phonebook>
+  <Titleh1>Phonebook</Titleh1>
+  <ContactForm />
+  <Titleh2>Contacts</Titleh2>
+  <Filter />
+  <ContactList />
+  <ToastContainer autoClose={2000} />
+</Phonebook>)
+}
+
